34cbccdd11632766a8eb9a9a5125a119ff3d9c7c
Jorge <83678281+krRyptt0m01@users.noreply.github.com>
2022-02-26 15:29:48 -0500

    Sidebar fixed

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ bar.onclick = function () //attach to onclick
   var checkbox = document.getElementById("check-toggle"); //find checkbox
 
   checkbox.checked = !checkbox.checked; //toggle the checked status
-  bar.classList.toggle("fa-bars");
-  bar.classList.toggle("fa-xmark");
+  if (checkbox.checked) {
+    bar.classList.remove("fa-bars");
+    bar.classList.add("fa-xmark");
+  } else {
+    bar.classList.add("fa-bars");
+    bar.classList.remove("fa-xmark");
+  }
 };
